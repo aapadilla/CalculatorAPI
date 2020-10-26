5a710f2274b8153731104e33ff31bda71e159b2d
Angel Padilla <padillaangel10@gmail.com>
2020-10-25 21:07:13 -0500

    * CalculatorResult.cs: * Orion.Calculator.Models.csproj:
    * CalculatorAPI.sln: Update/Cleanup/PostManCollection

---
+++ Orion.CalculatorModels/CalculatorResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Orion.Calculator.Models
+{
+    public class CalculatorResult
+    {
+        public bool IsSuccessful { get; set; }
+
+        public double Result { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
